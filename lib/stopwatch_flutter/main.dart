7d7aa6e24bb57bd48b0f07cf56f67da0a2086c16
adicomdotir <adicom.ir@gmail.com>
2022-12-11 10:49:19 +0330

    stop_watch init

---
+++ lib/stopwatch_flutter/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:widget_test_practice/stopwatch_flutter/ui/stopwatch.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stopwatch(),
+      ),
+    );
+  }
+}
