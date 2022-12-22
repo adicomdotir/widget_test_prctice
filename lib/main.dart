629c413e978debc9e22a83767825f9b88691946d
adicomdotir <adicom.ir@gmail.com>
2022-12-22 20:10:16 +0330

    WP: flexible vs expanded wigets

--- lib/main.dart
+++ lib/main.dart
@@ -22,36 +22,7 @@ final routeGenerate = {
   AppRoutes.register: (context) => const RegisterPage(),
 };
 
-class User {
-  final int id;
-  final String name;
-
-  User(this.id, this.name) {
-    'Constructor Called'.log();
-  }
-
-  @override
-  String toString() {
-    return 'User (id: $id, name: $name)';
-  }
-}
-
 void main() {
-  'Main Function Running'.log();
-  // sl.registerFactory<User>(() {
-  //   final id = Random().nextInt(8999) + 1000;
-  //   return User(id, 'User$id');
-  // });
-  sl.registerLazySingleton<User>(() {
-    return User(1, 'User1');
-  });
-
-  'User logging section'.log();
-  for (var i = 0; i < 10; i++) {
-    final user = sl<User>();
-    user.log();
-  }
-
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -68,37 +39,61 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    print(size);
+    print(AppBar().preferredSize.height);
+    print(MediaQuery.of(context).padding);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, AppRoutes.login);
-              },
-              child: Text(AppRoutes.login.toUpperCase()),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, AppRoutes.register);
-              },
-              child: Text(AppRoutes.register.toUpperCase()),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.grey,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height -
+              AppBar().preferredSize.height -
+              MediaQuery.of(context).padding.top,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Flexible(
+                    child: textWithContainer(Colors.red, '1'),
+                  ),
+                  Flexible(
+                    child: textWithContainer(Colors.green, '2'),
+                    fit: FlexFit.tight,
+                  ),
+                  Flexible(
+                    child: textWithContainer(Colors.blue, '3'),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: textWithContainer(Colors.redAccent, '1'),
+                  ),
+                  Expanded(
+                    child: textWithContainer(Colors.greenAccent, '2'),
+                  ),
+                  Expanded(
+                    child: textWithContainer(Colors.blueAccent, '3'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget textWithContainer(Color color, String txt) => Container(
+        color: color,
+        child: Text(txt),
+      );
 }
 
 class LoginPage extends StatelessWidget {
@@ -110,7 +105,7 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: const MyCustomForm(),
+      body: Container(),
     );
   }
 }
@@ -145,64 +140,6 @@ class RegisterPage extends StatelessWidget {
                 Navigator.pushReplacementNamed(context, AppRoutes.home);
               },
               child: Text(AppRoutes.home.toUpperCase()),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({Key? key}) : super(key: key);
-
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-class MyCustomFormState extends State<MyCustomForm> {
-  final _formKey = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context) {
-// Build Form widget using the _formKey created
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: 'Enter name',
-                labelText: 'Name',
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.phone),
-                hintText: 'Enter phone number',
-                labelText: 'Phone',
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.calendar_today),
-                hintText: 'Enter date of birth',
-                labelText: 'DOB',
-              ),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            Center(
-              child: ElevatedButton(
-                child: const Text('Submit'),
-                onPressed: () {},
-              ),
             ),
           ],
         ),
