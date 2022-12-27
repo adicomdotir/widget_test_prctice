309429c447101fe6f47e31a25afbbf3e715948ef
adicomdotir <adicom.ir@gmail.com>
2022-12-27 11:57:02 +0330

    Add provider 05 & 06 samples

--- lib/provider_sample/provider_05/main.dart
+++ lib/provider_sample/provider_05/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:widget_test_practice/provider_sample/provider_04/model/dog.dart';
+import 'package:widget_test_practice/provider_sample/provider_05/model/dog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     debugPrint('Build $runtimeType');
     return ChangeNotifierProvider<Dog>(
-      create: (context) => Dog(name: 'name04', breed: 'breed04'),
+      create: (context) => Dog(name: 'name05', breed: 'breed05'),
       child: MaterialApp(
-        title: 'Provider 04',
+        title: 'Provider 05',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
     debugPrint('Build $runtimeType');
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Provider 04'),
+        title: const Text('Provider 05'),
       ),
       body: Center(
         child: Column(
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '- name: ${Provider.of<Dog>(context, listen: false).name}',
+              '- name: ${context.watch<Dog>().name}',
               style: const TextStyle(fontSize: 20.0),
             ),
             const SizedBox(
@@ -70,7 +70,7 @@ class BreedAndAge extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- breed: ${Provider.of<Dog>(context, listen: false).breed}',
+          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
           style: const TextStyle(fontSize: 20.0),
         ),
         const SizedBox(
@@ -91,14 +91,14 @@ class Age extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- age: ${Provider.of<Dog>(context).age}',
+          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
           style: const TextStyle(fontSize: 20.0),
         ),
         const SizedBox(
           height: 20.0,
         ),
         ElevatedButton(
-          onPressed: () => Provider.of<Dog>(context, listen: false).grow(),
+          onPressed: () => context.read<Dog>().grow(),
           child: const Text(
             'Grow',
             style: TextStyle(fontSize: 20.0),
@@ -113,3 +113,4 @@ class Age extends StatelessWidget {
 // context.read<T>()
 // Provider.of<T>(context, listen: true)
 // context.watch<T>()
+// context.select<T, E>(E Function(T) func)
--- lib/provider_sample/provider_06/main.dart
+++ lib/provider_sample/provider_06/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:widget_test_practice/provider_sample/provider_04/model/dog.dart';
+import 'package:widget_test_practice/provider_sample/provider_06/model/babies.dart';
+import 'package:widget_test_practice/provider_sample/provider_06/model/dog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     debugPrint('Build $runtimeType');
-    return ChangeNotifierProvider<Dog>(
-      create: (context) => Dog(name: 'name04', breed: 'breed04'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Dog>(
+          create: (context) => Dog(name: 'name06', breed: 'breed06', age: 3),
+        ),
+        FutureProvider<int>(
+          initialData: 0,
+          create: (context) {
+            final int dogAge = context.read<Dog>().age;
+            final babies = Babies(age: dogAge);
+            return babies.getBabies();
+          },
+        )
+      ],
       child: MaterialApp(
-        title: 'Provider 04',
+        title: 'Provider 06',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -39,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
     debugPrint('Build $runtimeType');
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Provider 04'),
+        title: const Text('Provider 06'),
       ),
       body: Center(
         child: Column(
@@ -47,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '- name: ${Provider.of<Dog>(context, listen: false).name}',
+              '- name: ${context.watch<Dog>().name}',
               style: const TextStyle(fontSize: 20.0),
             ),
             const SizedBox(
@@ -70,7 +83,7 @@ class BreedAndAge extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- breed: ${Provider.of<Dog>(context, listen: false).breed}',
+          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
           style: const TextStyle(fontSize: 20.0),
         ),
         const SizedBox(
@@ -91,14 +104,21 @@ class Age extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- age: ${Provider.of<Dog>(context).age}',
+          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
+          style: const TextStyle(fontSize: 20.0),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          '- number of Babies: ${context.read<int>()}',
           style: const TextStyle(fontSize: 20.0),
         ),
         const SizedBox(
           height: 20.0,
         ),
         ElevatedButton(
-          onPressed: () => Provider.of<Dog>(context, listen: false).grow(),
+          onPressed: () => context.read<Dog>().grow(),
           child: const Text(
             'Grow',
             style: TextStyle(fontSize: 20.0),
@@ -113,3 +133,4 @@ class Age extends StatelessWidget {
 // context.read<T>()
 // Provider.of<T>(context, listen: true)
 // context.watch<T>()
+// context.select<T, E>(E Function(T) func)
