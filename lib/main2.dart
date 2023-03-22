2789c3a4afc985427a2a2e85f3682b8e7fed5bd9
adicomdotir <adicom.ir@gmail.com>
2023-03-22 14:22:26 +0330

    Add constraintBox sample

---
+++ lib/main2.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: NewWidget(),
+      ),
+    ),
+  );
+}
+
+class NewWidget extends StatefulWidget {
+  const NewWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<NewWidget> createState() => _NewWidgetState();
+}
+
+class _NewWidgetState extends State<NewWidget> {
+  String message = '0';
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 90,
+              minWidth: 45,
+              maxHeight: 90,
+            ),
+            child: Container(
+              color: Colors.blueAccent,
+              child: Text(message),
+            ),
+          ),
+          SizedBox(
+            height: 64.0,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final newNum = Random().nextInt(90) + 10;
+              message += ' $newNum';
+              setState(() {});
+            },
+            child: const Text('Add Random Number'),
+          ),
+        ],
+      ),
+    );
+  }
+}
