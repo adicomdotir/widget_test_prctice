a00ff10cdc0b010a10f8af167324b4520cee0b80
adicomdotir <adicom.ir@gmail.com>
2023-04-04 12:55:17 +0330

    WP: temprature_slider part01

---
+++ lib/ui_tutorials/temprature_slider/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: TemperatureView(),
+    );
+  }
+}
+
+class TemperatureView extends StatelessWidget {
+  final ValueNotifier<double> temperature = ValueNotifier(20.0);
+
+  TemperatureView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        child: ValueListenableBuilder(
+          valueListenable: temperature,
+          builder: (context, value, child) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Text('${temperature.value.toInt()}'),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.grey[300],
+                    child: Center(
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.loose(
+                          const Size(400, 800),
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Container(
+                                color: Colors.amberAccent,
+                                child: Text('1'),
+                              ),
+                            ),
+                            Positioned.fill(
+                              left: 300,
+                              child: Container(
+                                color: Colors.redAccent,
+                                child: Text('2'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
